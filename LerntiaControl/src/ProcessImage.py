8b114c7ee5dbca60329e1169c02c2bec4e666c21
Sarah El-Sherbiny <sarah.el-sherbiny@tuwien.ac.at>
2019-12-25 21:14:53 +0100

    Remove eyeball detection as it is too slow

--- LerntiaControl/src/ProcessImage.py
+++ LerntiaControl/src/ProcessImage.py
@@ -46,45 +46,8 @@ class ProcessImage:
             # cut face and eye out of the image
             cut_face = self.frame[y:y + h, x:x + w]
             cut_eye = cut_face[left_ey:left_ey + left_eh, left_ex:left_ex + left_ew]
-            print("a")
-            rows, cols, _ = cut_eye.shape
-            print("aa")
-            gray_roi = cv2.cvtColor(cut_eye, cv2.COLOR_BGR2GRAY)
-            # gray_roi = cv2.GaussianBlur(gray_roi, (2, 2), 0)
-            print("b")
-            _, threshold = cv2.threshold(gray_roi, 3, 255, cv2.THRESH_BINARY_INV)
-            print("c", threshold)
-            contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-            print("cc")
-            if contours:
-                contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
-                print("d")
-                for cnt in contours:
-                    (x, y, w, h) = cv2.boundingRect(cnt)
-                    cv2.drawContours(cut_eye, [cnt], -1, (0, 0, 255), 3)
-                    cv2.rectangle(cut_eye, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                    cv2.line(cut_eye, (x + int(w / 2), 0), (x + int(w / 2), rows), (0, 255, 0), 2)
-                    cv2.line(cut_eye, (0, y + int(h / 2)), (cols, y + int(h / 2)), (0, 255, 0), 2)
-                    break
-
-            # cv2.imshow("eye", gray_roi)
-            # cv2.imshow("threshold", threshold)
-            # cv2.waitKey(0)
-
-            # circles = cv2.HoughCircles(cut_eye, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=50,
-            #                                       minRadius=0, maxRadius=1)
-
-            # circles = np.uint16(np.around(circles))
-            # for i in circles[0, :]:
-            # outer circle
-            #    cv2.circle(cut_eye, (i[0], i[1]), i[2], (0, 255, 0), 2)
-            # circle center
-            #    cv2.circle(cut_eye, (i[0], i[1]), 2, (0, 0, 255), 3)
-
-            # cv2.imshow('circles', cut_eye)
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
-
+            if cut_eye.size:
+                cv2.imshow("cut_eye", cut_eye)
 
         print("left eye, x-coordinate: ", left_ex)
         return self.frame
