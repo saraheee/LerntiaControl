5484c226c017a6a67e2b9fdd4b478f8260417f4e
Sarah El-Sherbiny <sarah.el-sherbiny@tuwien.ac.at>
2020-01-18 03:27:21 +0100

    Enhance mouse movement and enable clicking #17 #11

--- LerntiaControl/src/MoveMouse.py
+++ LerntiaControl/src/MoveMouse.py
@@ -1,12 +1,10 @@
-import sys
-
 from pynput.mouse import Button, Controller
 
-x_sens = 15
-y_sens = 15
-x_step = 50
-y_step = 40
-numf = 20
+x_sens = 0.5
+y_sens = 0.5
+x_step = 1.1
+y_step = 0.8
+numf = 20  # frame number
 
 
 class MoveMouse:
@@ -25,80 +23,37 @@ class MoveMouse:
         if self.prev_data:
             last_frames = self.prev_data[-numf:]
 
-        # move mouse based on difference to gaze point
-            # smallest_value = sys.maxsize
-            # for f in last_frames:
-            #    if f.x_middle < smallest_value:
-            #        smallest_value = f.x_middle
-            # if smallest_value < self.data.x_middle - x_sens:
-            #    print("move right")
-            #    self.mouse.move(x_step, 0)
-
             # weighted difference values
-            xdifferences = []  # x - self.data.x_middle for x in last_frames]
-            ydifferences = []  # y - self.data.y_middle for y in last_frames]
+            x_differences = []
+            y_differences = []
 
             for d in last_frames:
-                # print("x_middle:", d.x_middle)
-                # print("y_middle:", d.y_middle)
-                xdifferences.append(self.data.x_middle - d.x_middle)
-                ydifferences.append(self.data.y_middle - d.y_middle)
+                x_differences.append(self.data.x_middle - d.x_middle)
+                y_differences.append(self.data.y_middle - d.y_middle)
 
-            # todo: consider x_sens and y_sens in difference
-            # print("xlen: ", len(xdifferences))
-            # print("ylen: ", len(ydifferences))
+            x_cond = []
+            y_cond = []
 
-            # for f in xdifferences:
-            #     print("xdiff: ", f)
+            if len(x_differences):
+                print("x len: ",  len(x_differences))
+                print("x val: ", sum(x_differences) / len(x_differences))
+                x_value = sum(x_differences) / len(x_differences) * x_step
+                x_cond = x_value > x_sens or x_value < -x_sens
+                if x_cond:
+                    self.mouse.move(x_value, 0)
 
-            # for f in ydifferences:
-            #     print("Y DIFF: ", f)
+            if len(y_differences):
+                print("y len: ", len(x_differences))
+                print("y val: ", sum(x_differences) / len(x_differences))
+                y_value = sum(y_differences) / len(y_differences) * y_step
+                y_cond = y_value > y_sens or y_value < -y_sens
+                if y_cond:
+                    self.mouse.move(0, y_value)
 
-        # CRASH START
-        #    if len(xdifferences):
-        #        self.mouse.move(sum(xdifferences) / len(xdifferences), 0)
-        #    if len(ydifferences):
-        #        self.mouse.move(0, sum(ydifferences) / len(ydifferences))
-        # CRASH END
-
-        # if self.prev_data:  # and self.prev_data.x_middle > self.data.x_middle + x_sens:
-        #    largest_value = -1
-        #    for f in last_frames:
-        #        if f.x_middle > largest_value:
-        #            largest_value = f.x_middle
-            # if largest_value > self.data.x_middle + x_sens:
-            #    print("move left")
-            #    self.mouse.move(-x_step, 0)
-
-        # if self.prev_data:
-        #    smallest_value = sys.maxsize
-        #    for f in last_frames:
-        #        if f.y_middle < smallest_value:
-        #            smallest_value = f.y_middle
-            # if smallest_value < self.data.y_middle - y_sens:
-            #    print("move up")
-            #    self.mouse.move(0, y_step)
-
-        # if self.prev_data:
-        #    largest_value = -1
-        #    for f in last_frames:
-        #        if f.y_middle > largest_value:
-        #            largest_value = f.y_middle
-            # if largest_value > self.data.y_middle + y_sens:
-            #    print("move down")
-            #    self.mouse.move(0, -y_step)
+            if len(x_differences) and len(y_differences):
+                if not x_cond and not y_cond:
+                    self.mouse.click(Button.left, 1)  # todo: change mouse and detect click gesture
 
     def detect_head_nod(self):
         pass  # todo
 
-        # print("x_middle point: ", self.data.x_middle)
-        # if self.prev_data:
-        #    print("x_middle point, previous: ", self.prev_data.x_middle)
-
-        # set mouse position
-        # self.mouse.position = (0, 0)
-        # move mouse
-        # self.mouse.move(100, 0)
-
-        # mouse left click
-        # self.mouse.click(Button.left, 1)
